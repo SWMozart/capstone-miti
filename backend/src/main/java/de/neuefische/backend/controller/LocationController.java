f40bda7cec1488fd4d340c3648163ffd21160d34
sebastianwitt <witt.seb@outlook.de>
2022-11-04 14:22:32 +0100

    BE: location map fix

--- backend/src/main/java/de/neuefische/backend/controller/LocationController.java
+++ backend/src/main/java/de/neuefische/backend/controller/LocationController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RequestMapping("/api/locations")
 public class LocationController {
 
-    private LocationService locationService;
+    private final LocationService locationService;
 
     @Autowired
     public LocationController(LocationService locationService) {
